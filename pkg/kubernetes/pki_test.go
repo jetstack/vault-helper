3123cd0c7915cc3579ecad35de55666bdfae5d9e
Joshua  Van Leeuwen <joshua.vanleeuwen@jetstack.io>
2017-07-12 17:39:37 +0100

    Added test for pki type

--- pkg/kubernetes/pki_test.go
+++ pkg/kubernetes/pki_test.go
@@ -7,6 +7,8 @@ import (
 	//"github.com/Sirupsen/logrus"
 	//"gitlab.jetstack.net/jetstack-experimental/vault-helper/pkg/kubernetes"
 
+	vault_testing "github.com/hashicorp/vault/api"
+
 	"gitlab.jetstack.net/jetstack-experimental/vault-helper/pkg/testing/vault_dev"
 )
 
@@ -70,6 +72,25 @@ func TestPKI_Ensure(t *testing.T) {
 	}
 	if !exists {
 		t.Error("Policy not found")
+	}
+
+	pkiWrongType := NewPKI(k, "wrong-type-pki")
+
+	err = k.vaultClient.Sys().Mount(
+		k.Path()+"/pki/"+"wrong-type-pki",
+		&vault_testing.MountInput{
+			Description: "Kubernetes " + k.clusterID + "/" + "wrong-type-pki" + " CA",
+			Type:        "generic",
+			Config:      k.etcdKubernetesPKI.getMountConfigInput(),
+		},
+	)
+	if err != nil {
+		t.Error("Error Mounting: ", err)
+	}
+
+	err = pkiWrongType.Ensure()
+	if err == nil {
+		t.Error("Should have error from wrong type")
 	}
 
 }
