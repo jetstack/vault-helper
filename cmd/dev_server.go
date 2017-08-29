3fa64255013c431444338a25c682425a2d5424c4
Joshua  Van Leeuwen <joshua.vanleeuwen@jetstack.io>
2017-08-29 16:44:18 +0100

    dev-server gives ready state to systemd
    After dev-server has finished setting up and is in a ready state, it
    will send a READY signal to notify systemd
    
    Closes #21

--- cmd/dev_server.go
+++ cmd/dev_server.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/spf13/cobra"
 
+	"github.com/coreos/go-systemd/daemon"
 	"github.com/jetstack-experimental/vault-helper/pkg/dev_server"
 )
 
@@ -19,7 +23,24 @@ var devServerCmd = &cobra.Command{
 			log.Fatalf("no cluster ID was given")
 		}
 
+		wait, err := cmd.PersistentFlags().GetBool(dev_server.FlagWaitSignal)
+		if err != nil {
+			log.Fatalf("error finding wait value: %v", err)
+		}
+
+		port, err := cmd.PersistentFlags().GetInt(dev_server.FlagPortNumber)
+		if err != nil {
+			log.Fatalf("error finding port value: %v", err)
+		}
+		if port > 65536 {
+			log.Fatalf("invalid port %d > 65536", port)
+		}
+		if port < 1 {
+			log.Fatalf("invalid port %d < 1", port)
+		}
+
 		v := dev_server.New(log)
+		v.Vault.SetPort(port)
 
 		if err := v.Run(cmd, args); err != nil {
 			log.Fatal(err)
@@ -29,6 +50,11 @@ var devServerCmd = &cobra.Command{
 			log.Infof(n + "-init_token := " + t)
 		}
 
+		daemon.SdNotify(false, "READY=1")
+
+		if wait {
+			waitSignal(v)
+		}
 	},
 }
 
@@ -43,4 +69,29 @@ func init() {
 	devServerCmd.PersistentFlags().String(dev_server.FlagInitTokenAll, "", "Set init-token-all    (Default to new token)")
 
 	RootCmd.AddCommand(devServerCmd)
+}
+
+func waitSignal(v *dev_server.DevVault) {
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	exit_chan := make(chan int)
+
+	go func() {
+		for {
+			s := <-signal_chan
+			switch s {
+			case syscall.SIGTERM:
+				exit_chan <- 0
+
+			case syscall.SIGQUIT:
+				exit_chan <- 0
+			}
+		}
+	}()
+
+	<-exit_chan
+	v.Vault.Stop()
 }
